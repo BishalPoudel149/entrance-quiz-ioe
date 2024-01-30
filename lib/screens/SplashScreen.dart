2df6bc6eef814e58bfddbad8c6b4635e97d7c2d0
bishalpoudel149 <bishalpoudel563@gmail.com>
2024-01-30 22:20:17 +0530

    Onboarding Page Implementation

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ientrance/main.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ientrance/screens/OnboardingScreen.dart';
+import 'package:ientrance/screens/Signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,27 +13,32 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    
-    Future.delayed(const Duration(seconds: 3), (){
+    Future.delayed(const Duration(seconds: 3), checkOnboardingAndNavigate);
+  }
+
+  Future<void> checkOnboardingAndNavigate() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    final bool onBoardingCompleted = pref.getBool("onboarding") ?? false;
+
+    if (!onBoardingCompleted) {
       Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-              builder: (_) => const MyHomePage(title: "IO_ TOPS"
-              )
-          )
-      );
-    });
+          MaterialPageRoute(builder: (_) => const OnboardingScreen()));
+    } else {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => const SignUp()));
+    }
   }
 
   @override
   void dispose() {
     super.dispose();
-    SystemChrome.setEnabledSystemUIMode(
-        SystemUiMode.manual,
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: SystemUiOverlay.values);
   }
 
@@ -46,27 +54,25 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
             end: Alignment.bottomLeft,
           ),
         ),
-        child:  Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                   Image.asset(
+                  Image.asset(
                     'assets/images/logo.png',
                     width: 160,
                     height: 160,
                     color: Colors.greenAccent,
                   ),
-                  const SizedBox(height: 20,),
-                  Text(
-                    "IO_ TOPS",
-                    style:  GoogleFonts.lato(
-                      fontSize: 60,
-                      fontWeight: FontWeight.bold
-                    )
-                  )
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text("IO_ TOPS",
+                      style: GoogleFonts.lato(
+                          fontSize: 60, fontWeight: FontWeight.bold))
                 ],
               ),
             ),
---
+++ lib/utils/Onboarding_items.dart
@@ -0,0 +1,21 @@
+import '../models/onboarding_info.dart';
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: "Comprehensive Exam Coverage",
+        description:
+            "Explore a vast range of subjects and topics essential for your exams, ensuring thorough preparation across all relevant areas of study.",
+        image: "assets/images/onboard1.gif"),
+    OnboardingInfo(
+        title: "Progress Tracking",
+        description:
+            "Monitor quiz scores closely, pinpointing areas for improvement and tracking your advancement through detailed performance analytics.",
+        image: "assets/images/onboard2.gif"),
+    OnboardingInfo(
+        title: "Interactive Learning ",
+        description:
+            "Engage with dynamic quizzes and enriching content, fostering an interactive and immersive learning environment tailored to your educational needs.",
+        image: "assets/images/onboard3.gif"),
+  ];
+}
