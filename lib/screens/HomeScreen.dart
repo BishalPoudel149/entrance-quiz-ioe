96bc31e52d108e5c2d259458df363bd50a614807
sudhirVoid <bhandarisudhir4274@gmail.com>
2024-02-04 12:26:17 +0530

    homepage implemented

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ientrance/widgets/button_custom.dart';
 
 class HomeScreen extends StatefulWidget {
   final String title;
@@ -11,68 +12,213 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void randomTopic() {}
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey.shade200,
         title: Row(
           children: [
-            // Logo and Title on the left
             Image.asset(
-              'assets/images/logo.png', // Replace with your logo image asset path
+              'assets/images/logo.png',
               width: 40,
               height: 40,
-              // Add any additional styling or constraints as needed
             ),
             SizedBox(width: 8),
             Text(
               'IO_ Tops',
               style: GoogleFonts.singleDay(
-                fontSize: 26,
-                fontWeight: FontWeight.bold
-              ),
+                  fontSize: 26, fontWeight: FontWeight.bold),
             ),
           ],
         ),
         actions: [
-          // Search Icon
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {
-              // Add your search functionality here
-            },
+            onPressed: () {},
           ),
-          // Notification Icon
           IconButton(
             icon: Icon(Icons.notifications),
-            onPressed: () {
-              // Add your notification functionality here
-            },
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              'Welcome Home  👋',
+              style: GoogleFonts.singleDay(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              height: 400,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple[100],
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Image.asset(
+                      'assets/images/think.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don\'t think. ',
+                            style: GoogleFonts.singleDay(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 19,
+                            ),
+                          ),
+                          Text(
+                            'Just Prepare',
+                            style: GoogleFonts.singleDay(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 34,
+                              color: Colors.deepPurple,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 265,),
+
+                      ButtonC(
+                        buttonText: 'Explore Random Topic',
+                        onTap: randomTopic,
+                        textColor: Colors.white,
+                        buttonColor: Colors.grey.shade600,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Top Collections',
+              style: GoogleFonts.singleDay(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Collection 1',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Collection 2',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Collection 3',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        backgroundColor: Colors.grey, // Set the background color to grey
+        selectedItemColor: Colors.blueGrey, // Set the selected item (icon and label) color to white
+        unselectedItemColor: Colors.grey, // Set the unselected item (icon and label) color to grey
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle),
+            label: 'Play',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create),
+            label: 'Create',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
         ],
       ),
 
-      body: Column(
-        children: [
-          SizedBox(height: 20,),
-          //top bar with logo and search func.
-
-        ],
-      ),
-    );
-  }
-}
-
-class CollectionItem extends StatelessWidget {
-  final String title;
-
-  CollectionItem({required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      leading: Icon(Icons.library_books),
     );
   }
 }
--- lib/screens/OtpScreen.dart
+++ lib/screens/OtpScreen.dart
@@ -99,6 +99,7 @@ class _OtpScreenState extends State<OtpScreen> {
         preferredSize: Size.fromHeight(80.0),
         child: SafeArea(
           child: AppBar(
+
             toolbarHeight: 80.0,
             leading: IconButton(
               icon: Icon(Icons.arrow_back, color: Colors.black),
@@ -113,13 +114,13 @@ class _OtpScreenState extends State<OtpScreen> {
               IconButton(
                 icon: Icon(
                   Icons.mail,
-                  color: Colors.white,
+                  color: Colors.yellow.shade600,
                   size: 34.0,
                 ),
                 onPressed: () {},
               ),
             ],
-            backgroundColor: Colors.purple,
+            backgroundColor: Colors.grey.shade200,
             centerTitle: true,
           ),
         ),
